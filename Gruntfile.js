e275dbb97429999aaea6d3cf6319609f48f1701c
blackmiaool <blackmiaool@gmail.com>
2015-09-03 20:26:06 +0800

    grunt config file finish

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,13 +17,13 @@ module.exports = function (grunt) {
                     collapseWhitespace: true
                 },
                 files: { // Dictionary of files
-                    'build/index.html': 'index.html'
+                    'build/index.html': 'build/index.html'
                 }
             }
         },
         zip: {
             'using-delate': {
-                src: ['build/index.html', 'build/script.js', 'build/style.css'],
+                src: 'build/index.html',
                 dest: 'build/files.zip',
                 compression: 'DEFLATE'
             },
@@ -45,35 +45,31 @@ module.exports = function (grunt) {
                 dest: 'build'
             }
         },
-        usemin: {
-            html: 'build/index.html',
-            options: {
-                
+        fixturesPath: ".",
+        htmlbuild: {
+            dist: {
+                src: 'index.html',
+                dest: 'build/index.html',
+                options: {
+                    relative: true,
+                    scripts: {
+                        bundle:'<%= fixturesPath %>/build/script.js',
+                    },
+                    styles: {
+                        bundle:'<%= fixturesPath %>/build/style.css',
+                    },
+                }
             }
-        }
+        },
     });
 
-    // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-zip');
-    grunt.loadNpmTasks('grunt-usemin');
-    grunt.loadNpmTasks('grunt-filerev');
-grunt.loadNpmTasks('grunt-html-build');
-    //    grunt.loadNpmTasks('grunt-contrib-filerev');
+    grunt.loadNpmTasks('grunt-html-build');
 
-    grunt.registerTask('build', [
-      'useminPrepare',
-      'concat:generated',
-      'cssmin:generated',
-      'uglify:generated',
-//      'filerev',
-      'usemin'
-    ]);
-    //    grunt.registerTask('default', ['uglify', 'htmlmin', 'cssmin', 'zip','useminPrepare']);
-    grunt.registerTask('default', ['build']);
+    grunt.registerTask('default', ['uglify','cssmin','htmlbuild','htmlmin','zip']);
 
 
 };
